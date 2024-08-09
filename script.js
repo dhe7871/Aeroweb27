f37c99994c8e02688a0f168377b05743e5744412
Dheeraj <prajapatidheeraj25042004@gmail.com>
2024-08-09 21:35:52 +0530

    adding book tab and visiter counter

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var clicks=0;
-let search=document.getElementById('search');
-let inputbox=document.getElementById('inputbox');
+const search=document.getElementById('search');
+const inputbox=document.getElementById('inputbox');
+const counter = document.getElementById('visiters');
+
+const p = fetch('https://api.counterapi.dev/v1/Aeroweb27/counter/up');
+p.then((response) =>{
+    if(!response.ok){
+        console.log('Error in fetching Visiter count...');
+    }
+    return response.json();
+}).then((response) =>{
+    counter.innerText = `Visiters: ${response.count}`;
+});
+
 search.addEventListener('click',searchbar);
 function searchbar(){
     if(!clicks){
@@ -12,48 +24,3 @@ function searchbar(){
         clicks=0;
     }
 }
-
-
-
-// // let searchArea=document.getElementsByClassName('search');
-// // var where=0;
-// for(let value of search){
-//     // if(where==0){
-//     //     searchArea[0].style.display="none";
-//     // }
-//     // value.addEventListener("mouseover",searchbar);
-    
-// }
-
-// for(let value of searchArea){
-    
-//     value.addEventListener("mouseout",nosearch);
-//     if(where==0){
-//         searchArea[0].style.display="none";
-//     }
-// }
-
-// function searchbar(){
-//     // let inputbox=document.createElement('div');
-//     // inputbox.setAttribute('id','inputbox');
-//     // inputbox.setAttribute('class','search ps-2 pe-2');
-//     // inputbox.innerHTML='<input type="text" placeholder="What are you looking for..."></input>';
-//     // inputbox.firstChild.style.width="25vw"
-//     // inputbox.firstChild.style.borderRadius="10px"
-//     // inputbox.style.position="relative";
-//     // inputbox.style.zIndex="10";
-//     // inputbox.style.backGroundColor="white"
-//     // search.insertAdjacentElement("beforebegin",inputbox);
-//     searchArea[0].style.display="inline-block"
-//     where=1;
-//     console.log(where);
-    
-// }
-// function nosearch(){
-//     // let inputbox=document.getElementById('inputbox');
-    
-//     // inputbox.remove();
-    
-//     where=0;
-//     console.log(where)
-// }
